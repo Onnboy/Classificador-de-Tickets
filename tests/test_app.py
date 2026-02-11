c97b726503c17262f74d640825c093336465fb2c
Onnboy <bomfimsantarosa@gmail.com>
2026-02-11 20:12:50 -0300

    test: implementa testes para health state check e tickets

---
+++ tests/test_app.py
@@ -0,0 +1,34 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from app.app import app
+
+
+def test_health_app():
+    client = TestClient(app)
+
+    response = client.get('/health')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'estado': 'EM ATIVIDADE'}
+
+
+def test_add_ticket_app_sucesso():
+    client = TestClient(app)
+
+    response = client.post(
+        '/v1/tickets/',
+        json={'titulo': 'Teste', 'descricao': 'Teste de Sucesso'},
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == 'Recebido'
+
+
+def test_add_ticket_app_error():
+    client = TestClient(app)
+
+    response = client.post('/v1/tickets/')
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+    response404 = client.post('/tickets/')
+    assert response404.status_code == HTTPStatus.NOT_FOUND
