0cc4a360e36bf5c1a567e8b0bd5c1fd9eb41d29d
Onnboy <bomfimsantarosa@gmail.com>
2026-04-19 16:53:13 -0300

    Corringo apenas para evitar 'line too long'

--- app/models/user.py
+++ app/models/user.py
@@ -5,16 +5,13 @@ from sqlmodel import Field, SQLModel
 
 
 class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-
+    id: Optional[int] = Field(
+        default=None, primary_key=True
+    )
     username: str = Field(index=True, unique=True)
-
     email: str = Field(unique=True)
-
     hashed_password: str
-
     is_active: bool = Field(default=True)
-
     criado_em: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc)
     )
